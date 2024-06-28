7c890f908900559ad48166540537a9ff7232b653
Hussain Rana <nothussainrana@icloud.com>
2024-06-29 01:56:32 +0800

    Database stuff

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 require 'functions.php';
-require 'Database.php';
 //require 'router.php';
+require 'Database.php';
 
-$db = new Database();
+$config = require('config.php');
+
+$db = new Database($config['database']);
 $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
-
 
 dd($posts);
