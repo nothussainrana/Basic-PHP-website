40f61d5ed0a49e62942f255c538ed643e5b1b209
Hussain Rana <nothussainrana@icloud.com>
2024-07-01 14:33:29 +0800

    Database stuff

--- index.php
+++ index.php
@@ -5,8 +5,11 @@ require 'functions.php';
 require 'Database.php';
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+$id = $_GET['id'];
+$query = "SELECT * FROM posts where id = ?";
+
+$posts = $db->query($query, [$id])->fetch();
 
 dd($posts);
