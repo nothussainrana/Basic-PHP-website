772bc3cb749855d43a7df2c70726e05e3b94bfd8
Hussain Rana <nothussainrana@icloud.com>
2024-07-04 16:51:42 +0800

    Database stuff

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,9 +1,8 @@
 <?php
 
-$config = require('config.php');
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$heading = "Note";
 $currentUserId = 1;
 
 $note = $db->query("SELECT * FROM notes WHERE id = :id" , [
@@ -12,4 +11,7 @@ $note = $db->query("SELECT * FROM notes WHERE id = :id" , [
 
 authorize($note['user_id'] === $currentUserId);
 
-require 'views/note.view.php';
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
