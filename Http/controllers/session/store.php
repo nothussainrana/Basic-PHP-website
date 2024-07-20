8e4f9aba10e91595d476f8f822fbffced689d244
Hussain Rana <nothussainrana@icloud.com>
2024-07-21 00:24:05 +0800

    Lots of refactoring, new classes, folder changes, making structure more readable

---
+++ Http/controllers/session/store.php
@@ -0,0 +1,21 @@
+<?php
+
+use Core\Authenticator;
+use Http\Forms\LoginForm;
+use Core\Session;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$form = new LoginForm();
+
+if ($form->validate($email, $password)) {
+    if ((new Authenticator)->attempt($email, $password)) {
+        redirect('/');
+    }
+    $form->error('email', 'No matching account found for that email and password.');
+}
+
+Session::flash('errors', $form->errors());
+
+return redirect('/login');
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\Session;
+
 session_start();
 
 const BASE_PATH = __DIR__ . '/../';
@@ -13,10 +15,12 @@ spl_autoload_register(function ($class) {
 require base_path('bootstrap.php');
 
 $router = new \Core\Router();
-
 $routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
+
+Session::unflash();
+var_dump($_SESSION);
